5b462b25acbfe9c0eec3176d57bbb2a6eeb3ab21
Krzysztof Kuchta <kkuchta@togetherdata.com>
2017-09-21 16:05:01 +0200

    Selector documentation.

--- selector/utils.py
+++ selector/utils.py
@@ -1,6 +1,38 @@
+"""
+    select_banner function should work as follow:
+    1) Select banners which are paid a lot.
+    2) Some percent of selected banners should be new banners without payments stats
+    3) The same user shoudn't take the same banners every time.
+
+"""
+
 def select_banner(request_data):
     """
-        Input
+        List of requests:
+         [
+            {
+                'id':'request_id'
+                'filters':{
+                    'required':[{
+                        'keyword':'banner_keyword',
+                        'filter':'banner_filter'
+                    },],
+                    'excluded':[{
+                        'keyword':'banner_keyword',
+                        'filter':'banner_filter'
+                    },]
+                },
+                'userid':'',
+                'keywords':impression_keywords
+            },
+        ]
 
+        Should return list of banners to display
+        [
+            {
+                'request_id':,
+                'banner_id':
+            },
+        ]
     """
     return []
