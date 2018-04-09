90350a8d52ae6f0112598aa8b2b58a49396b2d01
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-09 16:31:12 +0200

    Turning database operation into deferreds.

--- db/utils.py
+++ db/utils.py
@@ -1,41 +1,60 @@
 from adselect import db
+from twisted.internet import defer
 
 
-########################
-#### CAMPAIGNS #########
-########################
+# CAMPAIGNS #
+@defer.inlineCallbacks
 def get_campaign(campaign_id):
-    return db.get_campaign_collection().find_one({'campaign_id': campaign_id})
+    collection = yield db.get_campaign_collection()
+    return_value = yield collection.find_one({'campaign_id': campaign_id})
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def get_campaigns_iter():
-    return db.get_campaign_collection().find(cursor=True)
+    collection = yield db.get_campaign_collection()
+    return_value = yield collection.find(cursor=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def update_campaign(campaign_doc):
-    return db.get_campaign_collection().replace_one({'campaign_id': campaign_doc['campaign_id']},
-                                                    campaign_doc, upsert=True)
+    collection = yield db.get_campaign_collection()
+    return_value = yield collection.replace_one({'campaign_id': campaign_doc['campaign_id']},
+                                                campaign_doc, upsert=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def delete_campaign(campaign_id):
-    return db.get_campaign_collection().delete_many({'campaign_id': campaign_id})
+    collection = yield db.get_campaign_collection()
+    return_value = yield collection.delete_many({'campaign_id': campaign_id})
+    defer.returnValue(return_value)
 
 
-#########################
-##### BANNERS ###########
-#########################
+# BANNERS #
 
+
+@defer.inlineCallbacks
 def get_banners_iter():
-    return db.get_banner_collection().find(cursor=True)
+    banner_coll = yield db.get_banner_collection()
+    return_value = yield banner_coll.find(cursor=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def get_banner(banner_id):
-    return db.get_banner_collection().find_one({'banner_id': banner_id})
+    banner_coll = yield db.get_banner_collection()
+    return_value = yield banner_coll.find_one({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def update_banner(banner_doc):
-    return db.get_banner_collection().replace_one({'banner_id': banner_doc['banner_id']},
-                                                  banner_doc, upsert=True)
+    banner_coll = yield db.get_banner_collection()
+    return_value = yield banner_coll.replace_one({'banner_id': banner_doc['banner_id']},
+                                                 banner_doc, upsert=True)
+    defer.returnValue(return_value)
 
 
 def get_campaign_banners(campaign_id):
@@ -46,69 +65,99 @@ def delete_campaign_banners(campaign_id):
     return db.get_banner_collection().delete_many({'campaign_id': campaign_id})
 
 
-############################
-##### STATS ################
-############################
+# STATS #
 
-
-# IMPRESSION COUNTS
+# IMPRESSION COUNTS #
+@defer.inlineCallbacks
 def get_banner_impression_count(banner_id):
-    return db.get_impressions_stats_collection().find_one({'banner_id': banner_id})
+    collection = yield db.get_impressions_stats_collection()
+    return_value = yield collection.find_one({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def get_banner_impression_count_iter():
-    return db.get_impressions_stats_collection().find(cursor=True)
+    collection = yield db.get_impressions_stats_collection()
+    return_value = yield collection.find(cursor=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def update_banner_impression_count(banner_id, counts_per_publisher_dict):
-    impression_stats_collection = db.get_impressions_stats_collection()
-    return impression_stats_collection.replace_one({"banner_id": banner_id},
-                                                   {"banner_id": banner_id,
-                                                    "stats": counts_per_publisher_dict},
-                                                   upsert=True)
+    impression_stats_collection = yield db.get_impressions_stats_collection()
+    return_value = yield impression_stats_collection.replace_one({"banner_id": banner_id},
+                                                                 {"banner_id": banner_id,
+                                                                  "stats": counts_per_publisher_dict},
+                                                                 upsert=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def delete_banner_impression_count(banner_id):
-    return db.get_impressions_stats_collection().delete_many({'banner_id': banner_id})
+    collection = yield db.get_impressions_stats_collection()
+    return_value = yield collection.delete_many({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
-# PAYMENTS
+# PAYMENTS #
+@defer.inlineCallbacks
 def get_banner_payment(banner_id):
-    return db.get_payments_stats_collection().find_one({'banner_id': banner_id})
+    collection = yield db.get_impressions_stats_collection()
+    return_value = yield collection.find_one({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def get_banner_payment_iter():
-    return db.get_payments_stats_collection().find(cursor=True)
+    collection = yield db.get_payments_stats_collection()
+    return_value = yield collection.find(cursor=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def update_banner_payment(banner_id, pay_per_publisher_per_size_per_keyword_dict):
-    payments_stats_collections = db.get_payments_stats_collection()
-    return payments_stats_collections.replace_one({"banner_id": banner_id},
-                                                  {"banner_id": banner_id,
-                                                   "stats": pay_per_publisher_per_size_per_keyword_dict},
-                                                  upsert=True)
+    collection = yield db.get_payments_stats_collection()
+    return_value = yield collection.replace_one({"banner_id": banner_id},
+                                                {"banner_id": banner_id,
+                                                 "stats": pay_per_publisher_per_size_per_keyword_dict},
+                                                upsert=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def delete_banner_payments(banner_id):
-    return db.get_payments_stats_collection().delete_many({'banner_id': banner_id})
+    collection = yield db.get_payments_stats_collection()
+    return_value = yield collection.delete_many({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
-# SCORES
+# SCORES #
+@defer.inlineCallbacks
 def get_banner_scores(banner_id):
-    return db.get_scores_stats_collection().find_one({'banner_id': banner_id})
+    collection = yield db.get_scores_stats_collection()
+    return_value = yield collection.find_one({'banner_id': banner_id})
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def get_banner_scores_iter():
-    return db.get_scores_stats_collection().find(cursor=True)
+    collection = yield db.get_scores_stats_collection()
+    return_value = yield collection.find(cursor=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def update_banner_scores(banner_id, score_per_publisher_per_keyword_dict):
-    score_stats_collection = db.get_scores_stats_collection()
-    return score_stats_collection.replace_one({'banner_id': banner_id},
-                                              {'banner_id': banner_id,
-                                               'stats': score_per_publisher_per_keyword_dict},
-                                              upsert=True)
+    collection = yield db.get_scores_stats_collection()
+    return_value = yield collection.replace_one({'banner_id': banner_id},
+                                                {'banner_id': banner_id,
+                                                 'stats': score_per_publisher_per_keyword_dict},
+                                                upsert=True)
+    defer.returnValue(return_value)
 
 
+@defer.inlineCallbacks
 def delete_banner_scores(banner_id):
-    return db.get_scores_stats_collection().delete_many({'banner_id': banner_id})
+    collection = yield db.get_scores_stats_collection()
+    return_value = yield collection.delete_many({'banner_id': banner_id})
+    defer.returnValue(return_value)
