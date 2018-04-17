cb656b3840647efe8e9d164ba0ac2c029b3125ec
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-17 15:13:46 +0200

    Python 3 friendly print.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+from __future__ import print_function
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
@@ -22,9 +23,9 @@ sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../../../'))
 
 # Sphinx / readthedocs output
-print "sys.paths:"
+print("sys.paths:")
 for sp in sys.path:
-    print sp
+    print(sp)
 
 # -- Project information -----------------------------------------------------
 
