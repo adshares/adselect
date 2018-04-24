153be53a458dfee4548380c3b8aeffc6b1030e2b
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-24 21:24:01 +0200

    Move mocks to init.

--- tests/test_db_init.py
+++ tests/test_db_init.py
@@ -1,53 +1,25 @@
-import re
-import time
-
-from mock import patch, Mock, MagicMock, PropertyMock
-
-from twisted.trial import unittest
 from twisted.internet import defer
-import txmongo
 
-import adselect
 from adselect import db
 
+from tests import MockDBTestCase
 
-class DbInitTestCase(unittest.TestCase):
 
-    def setUp(self):
-
-        adselect.db.MONGO_CONNECTION = None
-
-        self.banner_collection = MagicMock()
-
-        self.mock_database = MagicMock()
-        self.mock_database.banner = self.banner_collection
-
-        self.mock_connection = MagicMock()
-        self.mock_connection.adselect = self.mock_database
-        self.mock_connection.disconnect.return_value = True
-
-        self.mock_lazyMongoConnectionPool = MagicMock()
-        self.mock_lazyMongoConnectionPool.return_value = self.mock_connection
-
-        self.original_lazyMongoConnectionPool = txmongo.lazyMongoConnectionPool
-        self.patch(txmongo, 'lazyMongoConnectionPool', self.mock_lazyMongoConnectionPool)
-
-    def tearDown(self):
-        txmongo.lazyMongoConnectionPool = self.original_lazyMongoConnectionPool
+class DbInitTestCase(MockDBTestCase):
 
     def test_get_mongo_db(self):
 
-        database = adselect.db.get_mongo_db()
+        database = db.get_mongo_db()
         self.assertIs(database, self.mock_database)
 
     def test_get_mongo_connection(self):
 
-        self.assertIsNone(adselect.db.MONGO_CONNECTION)
+        self.assertIsNone(db.MONGO_CONNECTION)
 
         connection = db.get_mongo_connection()
 
         self.assertEqual(connection, self.mock_lazyMongoConnectionPool.return_value)
-        self.assertIs(connection, adselect.db.MONGO_CONNECTION)
+        self.assertIs(connection, db.MONGO_CONNECTION)
 
     def test_get_collection(self):
 
@@ -61,14 +33,14 @@ class DbInitTestCase(unittest.TestCase):
     @defer.inlineCallbacks
     def test_disconnect(self):
 
-        self.assertIsNone(adselect.db.MONGO_CONNECTION)
+        self.assertIsNone(db.MONGO_CONNECTION)
 
         disconnected = yield db.disconnect()
         self.assertIsNone(disconnected)
 
         connection = db.get_mongo_connection()
         self.assertIsNotNone(connection)
-        self.assertIsNotNone(adselect.db.MONGO_CONNECTION)
+        self.assertIsNotNone(db.MONGO_CONNECTION)
 
         disconnected = yield db.disconnect()
         self.assertIsNone(disconnected)
