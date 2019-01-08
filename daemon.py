6cdc630a9ec9118685dd333c18aa7f9379777340
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-01-08 15:00:54 +0100

    Update logging configuration.

--- daemon.py
+++ daemon.py
@@ -1,21 +1,14 @@
-import json
-import logging.config
-import os
-
 from twisted.internet import reactor
 
 from adselect import db
+from adselect.contrib import logs as server_logging
 from adselect.iface import server as iface_server
 from adselect.stats import tasks as stats_tasks, utils as stats_utils
 
 if __name__ == "__main__":
 
-    logging.basicConfig()
-
-    logfile_path = os.path.join(os.getenv('ADSELECT_LOG_CONFIG_FILE', 'config/log_config.json'))
-
-    with open(logfile_path, "r") as fd:
-        logging.config.dictConfig(json.load(fd))
+    # Set up logging.
+    server_logging.setup()
 
     # Configuring database.
     db.configure_db()
@@ -26,7 +19,7 @@ if __name__ == "__main__":
     # Initializing periodic tasks to recalculate scores.
     stats_tasks.configure_tasks()
 
-    # Start http interface to communicate with others Adshares components.
+    # Start http interface to communicate with others AdShares components.
     iface_server.configure_iface()
 
     # Run.
