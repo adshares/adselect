06157f6b554272731b749ff7c1c6bf180905b9df
Jacek Zemło <jacek@zel.pl>
2019-02-28 13:51:42 +0100

    Listen interface

--- adselect/iface/const.py
+++ adselect/iface/const.py
@@ -2,6 +2,7 @@ import os
 
 #: Twisted TCP port number
 SERVER_PORT = int(os.getenv('ADSELECT_SERVER_PORT', 8011))
+SERVER_INTERFACE = os.getenv('ADSELECT_SERVER_INTERFACE', '127.0.0.1')
 
 #: JSONRPC error code for invalid data objects
 INVALID_OBJECT = -32010
