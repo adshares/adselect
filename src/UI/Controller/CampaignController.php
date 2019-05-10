5ead107e4e907bdb35488d3c6f7c935162389339
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-05-10 17:11:12 +0200

    Remove campaigns

--- src/UI/Controller/CampaignController.php
+++ src/UI/Controller/CampaignController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace Adshares\AdSelect\UI\Controller;
 
+use Adshares\AdSelect\Application\Dto\CampaignDeleteDto;
 use Adshares\AdSelect\Application\Dto\CampaignUpdateDto;
 use Adshares\AdSelect\Application\Exception\ValidationDtoException;
 use Adshares\AdSelect\Application\Service\CampaignUpdater;
@@ -39,6 +40,25 @@ class CampaignController
         }
 
         $this->campaignUpdater->update($dto->getCampaignCollection());
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+    public function delete(Request $request): JsonResponse
+    {
+        $content = json_decode($request->getContent(), true);
+
+        if ($content === null || !isset($content['campaigns'])) {
+            throw new BadRequestHttpException('Incorrect data');
+        }
+
+        try {
+            $dto = new CampaignDeleteDto($content['campaigns']);
+        } catch (ValidationDtoException $exception) {
+            throw new BadRequestHttpException($exception->getMessage());
+        }
+
+        $this->campaignUpdater->delete($dto->getIdCollection());
 
         return new JsonResponse([], Response::HTTP_NO_CONTENT);
     }
---
+++ tests/Application/Dto/CampaignDeleteDtoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Adshares\AdSelect\Tests\Application\Dto;
+
+use Adshares\AdSelect\Application\Dto\CampaignDeleteDto;
+use Adshares\AdSelect\Application\Exception\ValidationDtoException;
+use PHPUnit\Framework\TestCase;
+
+final class CampaignDeleteDtoTest extends TestCase
+{
+    public function testWhenIdsAreCorrectAndDuplicate(): void
+    {
+        $ids = [
+            '00000000000000000000000000000001',
+            '00000000000000000000000000000002',
+            '00000000000000000000000000000002',
+            '00000000000000000000000000000002',
+            '00000000000000000000000000000001',
+            '00000000000000000000000000000003',
+        ];
+
+        $dto = new CampaignDeleteDto($ids);
+
+        $collection = $dto->getIdCollection();
+
+        $this->assertCount(3, $collection);
+        $this->assertEquals('00000000000000000000000000000001', $collection[0]);
+        $this->assertEquals('00000000000000000000000000000002', $collection[1]);
+        $this->assertEquals('00000000000000000000000000000003', $collection[2]);
+    }
+
+    public function testWhenIdIsNotCorrect(): void
+    {
+        $this->expectException(ValidationDtoException::class);
+
+        $ids = [
+            '11111111',
+            '22222222',
+        ];
+
+        new CampaignDeleteDto($ids);
+    }
+}
