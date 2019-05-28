44f65c520f061a10a80bc8fd49941065e616e70a
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-05-28 11:46:57 +0200

    SEL-26 Endpoints to return last event id (paid and unpaid)

---
+++ src/Application/Service/EventFinder.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Adshares\AdSelect\Application\Service;
+
+use Adshares\AdSelect\Application\Dto\FoundEvent;
+
+interface EventFinder
+{
+    public function findLastUnpaidEvent(): FoundEvent;
+
+    public function findLastPaidEvent(): FoundEvent;
+}
