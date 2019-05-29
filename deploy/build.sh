9237579df11c794289dcf62cf5dedfea892d8907
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-05-29 14:31:11 +0200

    Deploy

---
+++ deploy/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Usage: build.sh [<location-of-functions-file-to-include> [<work-dir>]]
+[[ -z ${1:-""} ]] && set -eu || source ${1}/_functions.sh --vendor
+cd ${2:-"."}
+
+composer install --no-dev
+
+./bin/console cache:clear
+./bin/console ops:es:create-index  --force
