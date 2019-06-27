63aee6718d765e156e30981900130c0995d63744
Przemyslaw Furtak <przemyslawf@gmail.com>
2019-06-27 15:15:19 +0200

    New score formula based on max_cpc and budget values

--- src/Infrastructure/ElasticSearch/QueryBuilder/QueryBuilder.php
+++ src/Infrastructure/ElasticSearch/QueryBuilder/QueryBuilder.php
@@ -12,25 +12,37 @@ class QueryBuilder
     private $userHistory;
     /** @var QueryInterface */
     private $query;
+    /** @var int */
+    private $scoreThreshold;
 
-    public function __construct(QueryInterface $query, array $userHistory = [])
+    public function __construct(QueryInterface $query, int $scoreThreshold, array $userHistory = [])
     {
         $this->userHistory = $userHistory;
         $this->query = $query;
+        $this->scoreThreshold = $scoreThreshold;
     }
 
     public function build(): array
     {
+        $scriptScore = <<<PAINLESS
+        
+            long min = Long.min(params.score_threshold * doc.max_cpm[0], doc.budget[0]);
+            ((1 - Math.random()) * min) / (params.last_seen.containsKey(doc._id[0]) ? (params.last_seen[doc._id[0]] + 1) : 1)
+PAINLESS;
+
         return [
             'function_score' => [
                 'boost_mode' => 'replace',
                 'query' => $this->query->build(),
                 'script_score' => [
+
+
                     'script' => [
                         'lang' => 'painless',
-                        'source' => '(Math.random() + 0.5) / (params.last_seen.containsKey(doc._id[0]) ? (params.last_seen[doc._id[0]] + 1) : 1)',
+                        'source' => $scriptScore,
                         'params' => [
                             'last_seen' => (object)$this->userHistory,
+                            'score_threshold' => $this->scoreThreshold,
                         ],
                     ],
                 ],
