46d24cd06d8fc930c8aed1366edd2d6931a625d5
Jacek Zemło <jacek@zel.pl>
2021-02-19 16:22:48 +0100

    crontab update

--- deploy/crontablist.sh
+++ deploy/crontablist.sh
@@ -4,7 +4,11 @@ set -eu
 SERVICE_DIR=${SERVICE_DIR:-$(dirname "$(dirname "$(readlink -f "$0")")")}
 
 echo -n "11 * * * * "
-echo -n "php ${SERVICE_DIR}/bin/console ops:es:update-stats"
+echo -n "nice php ${SERVICE_DIR}/bin/console ops:es:update-stats -t 4"
+echo ""
+
+echo -n "*/5 * * * * "
+echo -n "nice php ${SERVICE_DIR}/bin/console ops:es:update-exp"
 echo ""
 
 echo -n "19 3 * * * "
